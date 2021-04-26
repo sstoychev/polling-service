1dbf5f6ebde1e29f6241e96380befbdf7412af97
Stoycho Stoychev <registration@sstoychev.name>
2021-04-26 07:33:54 +0200

    basic security

--- public/index.php
+++ public/index.php
@@ -1,4 +1,10 @@
 <?php
 
-require __DIR__ . '/../vendor/autoload.php';
 
+if ($_SERVER['REQUEST_URI'] == '/leaderboard/start') {
+    require __DIR__ . '/../src/start.php';
+} elseif ($_SERVER['REQUEST_URI'] == '/leaderboard/stop') {
+    require __DIR__ . '/../src/stop.php';
+} else {
+    echo 'Unknown request';
+}
